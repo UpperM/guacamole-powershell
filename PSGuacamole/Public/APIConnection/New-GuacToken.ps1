d917214c31288ba5bf50f6eb81696fe38648908b
UPPERM <courtois.matthieu.92@gmail.com>
2020-05-24 00:46:18 +0200

    Request password in a secure way
    https://github.com/UpperM/guacamole-powershell/issues/3

--- PSGuacamole/Public/APIConnection/New-GuacToken.ps1
+++ PSGuacamole/Public/APIConnection/New-GuacToken.ps1
@@ -12,7 +12,7 @@ Function New-GuacToken()
         [ValidateNotNullOrEmpty()]
         [Parameter(
             Position = 1,
-            Mandatory = $true
+            Mandatory = $False
         )]
         [System.String]
         $Password,
@@ -28,6 +28,21 @@ Function New-GuacToken()
 
     begin
     {
+        if ($Null -eq $Password -or $Password.Length -eq 0)
+        {
+            $SecurePassword = Read-Host "Enter password" -AsSecureString
+
+            # Decode SecureString to Plain text for Guacamole (https://www.powershelladmin.com/wiki/Powershell_prompt_for_password_convert_securestring_to_plain_text)
+            # Create a "password pointer"
+            $PasswordPointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+
+            # Get the plain text version of the password
+            $Password = [Runtime.InteropServices.Marshal]::PtrToStringAuto($PasswordPointer)
+
+            # Free the pointer
+            [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($PasswordPointer)
+        }
+
         $Body = @{
             username = $Username
             password = $Password
